5b18b2a9b72d2d9de6c94fb2e4621816665b2459
Granpino <granpino@juno.com>
2020-05-19 07:08:46 +0000

    upload
    new upload

---
+++ shortcut.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#create shortcut
+
+
+# create shortcut on desktop
+ 
+touch weather.desktop
+cat <<EOF > weather.desktop
+
+#!/usr/bin/bash
+
+[Desktop Entry]
+Name=Openweather
+Type=Application
+Exec=lxterminal -t "WEATHER" --working-directory=/home/pi/raspi_openweather/ -e ./weather.sh
+Icon=/home/pi/raspi-openweather/logo.png
+Comment=test
+Terminal=true
+
+EOF
+
+sudo chmod 755 weather.desktop
+sudo mv weather.desktop /home/pi/Desktop
