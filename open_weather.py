408a8f75b54b7400d48642494489d0f7dc4a1693
Granpino <granpino@juno.com>
2020-08-07 21:24:47 -0500

    Update
    Update. Cleaned some of the code.

--- open_weather.py
+++ open_weather.py
@@ -1,7 +1,9 @@
 ##
 #!/usr/bin/python
 # raspi openweather by Granpino. May 2020
-# Rev1
+# For a 3.5" or 5" screen with resolution set at 640x320  
+# The /boot/config.txt file needs to be changed first.
+# Rev1.1
 import sys, pygame
 from pygame.locals import *
 import time
@@ -12,57 +14,50 @@ import requests
 pygame.init()
 #==================== change these settings ===============
 settings = {
-    'api_key':'91118451a271265xxxxxxxxxxxxx',
-    'lat':'30.5602',
-    'lon':'-50.4471',
+    'api_key':'xxxxxx',
+    'lat':'42.5602',
+    'lon':'-88.4471',
     'temp_unit':'imperial'} #unit can be metric, or imperial
 #============================================================
 Temp_Unit = settings["temp_unit"]
 BASE_URL = "http://api.openweathermap.org/data/2.5/onecall?appid={0}&exclude=minutely,hourly&lat={1}&lon={2}&units={3}"
 
 ###setup
+#degSymF = unichr(0x2109)         # Unicode for Degree F
 degSYM = unichr(0x00B0)          #unicode for degree symbol
-pin = '4'  #dht22
+#degSymC = unichr(0x2103)
+pin = '4' 
 sensor = Adafruit_DHT.DHT22
 
 #set size of the screen
-size = width, height = 480, 320  #To fit in a small screen
+size = width, height = 640, 430  # to fit in a 3.5" screen
 
 #screen = pygame.display.set_mode(size) # use this for troubleshooting
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 
 #define colors
 cyan = 50, 255, 255
-blue = 0, 0, 255
+blue = 130, 75, 200
 black = 0, 0, 0
 white = 255, 255, 255
 lblue = 75, 140, 200
-green = 0, 255, 0
 silver = 192, 192, 192
-yellow = 255, 255, 0
+green = 0, 255, 0
 
 #other
+temp = 0
 localH = 0
 localT = 0
 
 clock = pygame.time.Clock()
-
-def connect_screen(): # connection dropouts
-    screen.fill(blue)
-    font=pygame.font.Font(None,25)
-    label=font.render("Trying to reconnect....", 1, (white))
-    screen.blit(label,(10,15))
-    pygame.display.flip()
-    time.sleep(6)
-    update_weather()
 
 def __del__(self):
 	"Destructor to make sure pygame shuts down"
 #define function that checks for mouse clicks
 def on_click():
     print('clicked')
-    #   exit has been pressed
-    if 396 < click_pos[0] < 458 and 1 < click_pos[1] < 56:  
+    #   exit has been pressed at upper right corner
+    if 531 < click_pos[0] < 600 and 20 < click_pos[1] < 70:  
 	button(0)
 
 #define action on pressing buttons
@@ -105,11 +100,8 @@ def update_weather():
     global localH
 
     final_url = BASE_URL.format(settings["api_key"],settings["lat"],settings["lon"],settings["temp_unit"])
-#    try:
+
     weather_data = requests.get(final_url).json()
-  #  except:
-   #     print "===connection error==="
-    #connect_screen()
     response = requests.get(final_url)
     x = response.json()
 
@@ -119,8 +111,8 @@ def update_weather():
     temp1 = round(temp1, 1)
     feels_like = y["feels_like"]
     humi1 = y["humidity"]
-    sunrise1 = y["sunrise"]
-    sunset = y["sunset"]
+   # sunrise1 = y["sunrise"]
+   # sunset = y["sunset"]
 
     z = y["weather"]
     description1 = z[0]["description"]
@@ -129,7 +121,7 @@ def update_weather():
     u = x["daily"] 
     date2 = u[0]["dt"]  #todays date
     humi2 = u[0]["humidity"]
-    sunrise2 = u[0]["sunrise"]  # not used
+   # sunrise2 = u[0]["sunrise"]  # not used
 
     aa = u[0]["temp"] 
     temp2 = aa["day"] # Temp
@@ -146,7 +138,7 @@ def update_weather():
     date3 = time.localtime(date3) # convert from unix time  
     DATE3 = time.strftime('%A', date3) # day of the week
     humi3 = u[1]["humidity"]
-    sunrise3 = u[1]["sunrise"]
+   # sunrise3 = u[1]["sunrise" # not used]
 
     aa = u[1]["temp"]
     temp3 = aa["day"]
@@ -162,7 +154,7 @@ def update_weather():
     date4 = time.localtime(date4) # convert from unix time  
     DATE4 = time.strftime('%A', date4)
     humi4 = u[2]["humidity"]
-    sunrise4 = u[2]["sunrise"]
+   # sunrise4 = u[2]["sunrise"]
 
     aa = u[2]["temp"]
     temp4 = aa["day"]
@@ -195,12 +187,12 @@ def update_weather():
 
 #===================    
 
-# SETUP
-sfont = pygame.font.SysFont('sans', 16, bold=0)
-mfont = pygame.font.SysFont('sans', 18, bold=1)
-m2font = pygame.font.SysFont('sans', 22, bold=1)
-lfont = pygame.font.SysFont('sans', 30, bold=1)
-xlfont = pygame.font.SysFont('sans', 60, bold=1)
+# Fonts
+sfont = pygame.font.SysFont('sans', 18, bold=0)
+mfont = pygame.font.SysFont('sans', 24, bold=1)
+m2font = pygame.font.SysFont('sans', 29, bold=1)
+lfont = pygame.font.SysFont('sans', 40, bold=1)
+xlfont = pygame.font.SysFont('sans', 80, bold=1)
 
 if Temp_Unit == ("imperial"):  # do not change this
 	degSym = unichr(0x2109)		# Unicode for DegreeF
@@ -209,6 +201,8 @@ else:
 
 
 def refresh_screen():
+    global tim2
+    global tim3
     tim1 = time.strftime( "%a, %b %d", time.localtime() )
     tim2 = time.strftime( "%I:%M", time.localtime() )
     tim3 = time.strftime( "%S", time.localtime() )
@@ -217,7 +211,7 @@ def refresh_screen():
     time_lbl2 = lfont.render(tim2, 1, (white))
     time_lbl3 = sfont.render(tim3, 1, (white))
 
-    skin = pygame.image.load("backgnd.png")
+    skin = pygame.image.load("640x429.png")
 
     screen.blit(skin,(0,0))
 
@@ -249,61 +243,68 @@ def refresh_screen():
     descrip4_lbl = sfont.render(description4, 1, white)
     minmax4_lbl = m2font.render(str(max4)+degSYM +"  /  "+ str(min4)+ degSYM, 1, white)
 
-    screen.blit(time_lbl1,(10, 17))
-    screen.blit(time_lbl2, (200,10))
-    screen.blit(time_lbl3,(280, 10))
-    screen.blit(outsideT_lbl, (30, 52))
-    screen.blit(outsideH_lbl, (40, 119))
-    screen.blit(outside_lbl, (57, 145))
-    screen.blit(descrip1_lbl, (180, 145))
-    screen.blit(degree_lbl, (150, 57))
-    screen.blit(localT_lbl, (320, 52))
-    screen.blit(localH_lbl, (345, 119))
-    screen.blit(inside_lbl, (370, 145))
-    screen.blit(degree_lbl, (440, 57))
-    screen.blit(load_icon1, (200, 45))
-    screen.blit(load_icon2, (40, 180))
-    screen.blit(load_icon3, (200, 180))
-    screen.blit(load_icon4, (360, 180))
-    screen.blit(day1_lbl, (45, 188))
-    screen.blit(descrip2_lbl, (20, 295))
-    screen.blit(minmax2_lbl, (15, 270))
-    screen.blit(day2_lbl, (200, 188))
-    screen.blit(descrip3_lbl, (180, 295))
-    screen.blit(minmax3_lbl, (175, 270))
-    screen.blit(day3_lbl, (360, 188))
-    screen.blit(descrip4_lbl, (340, 295))
-    screen.blit(minmax4_lbl, (335, 270))
-    screen.blit(logo, (400, 9))
+    screen.blit(time_lbl1,(13, 23))
+    screen.blit(time_lbl2, (264,13))
+    screen.blit(time_lbl3,(375, 13))
+    screen.blit(outsideT_lbl, (40, 80))
+    screen.blit(outsideH_lbl, (53, 164))
+    screen.blit(outside_lbl, (78, 199))
+    descript1_rect = descrip1_lbl.get_rect(center=(320, 200))
+    screen.blit(descrip1_lbl, descript1_rect)
+    screen.blit(degree_lbl, (204, 79))
+    screen.blit(localT_lbl, (421, 80))
+    screen.blit(localH_lbl, (458, 164))
+    screen.blit(inside_lbl, (492, 199))
+    screen.blit(degree_lbl, (584, 83))
+    screen.blit(load_icon1, (280, 99))
+    screen.blit(load_icon2, (68, 274))
+    screen.blit(load_icon3, (277, 277))
+    screen.blit(load_icon4, (497, 277))
+    day1_rect = day1_lbl.get_rect(center=(105, 264))
+    screen.blit(day1_lbl, day1_rect)
+    descript2_rect = descrip2_lbl.get_rect(center=(105, 410))
+    screen.blit(descrip2_lbl, descript2_rect)
+    screen.blit(minmax2_lbl, (24, 362))
+    day2_rect = day2_lbl.get_rect(center=(320, 264))
+    screen.blit(day2_lbl, day2_rect)
+    descript3_rect = descrip3_lbl.get_rect(center=(320, 410))
+    screen.blit(descrip3_lbl, descript3_rect)
+    screen.blit(minmax3_lbl, (236, 363))
+    day3_rect = day3_lbl.get_rect(center=(535, 264))
+    screen.blit(day3_lbl, day3_rect)
+    descript4_rect = descrip4_lbl.get_rect(center=(535, 410))
+    screen.blit(descrip4_lbl, descript4_rect)
+    screen.blit(minmax4_lbl, (444, 363))
+    screen.blit(logo, (531, 23))
 
-    pygame.draw.line(screen, white,(0,40),(478,40)) # horizontal
-    pygame.draw.line(screen, white,(160,190),(160,315))
-    pygame.draw.line(screen, white,(320,190),(320,315))
-    pygame.draw.line(screen, white,(300,50),(300,150))
+    pygame.draw.line(screen, white,(0,53),(637,53)) # horizontal
+    pygame.draw.line(screen, white,(400,69),(400,199))
+    pygame.draw.line(screen, white,(214,253),(214,420))
+    pygame.draw.line(screen, white,(426,251),(426,420))
+
+    time.sleep(.1)
 
     pygame.display.flip()
 
 def main():
-    timer = pygame.time.get_ticks()
     global click_pos
+    timer = pygame.time.get_ticks()
     while True:
         seconds=(pygame.time.get_ticks() - timer)/1000
-        if seconds > 240: # check every 4 min 
+        if seconds > 250: # check every 4 min 
 	    timer = pygame.time.get_ticks()
             update_weather() # update indoor and outdoor
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: #click on logo
                 click_pos = pygame.mouse.get_pos()
-                print "screen clicked" 
                 print click_pos 
                 on_click()
 
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE: # ESC to exit
                 sys.exit()
-        clock.tick(15) #refresh screen 
+        clock.tick(20) #fps
         refresh_screen()
 
 update_weather()
 main()
-
